a5b736c6f96d5aed2d92134dac2f8b55a3ba896d
Erik Tyler Villoria <evilloria@gmail.com>
2016-08-31 23:26:39 +0000

    updated url for heroku

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ var config = {};
 
 config.db = {};
 // the URL shortening host - shortened URLs will be this + base58 ID
-// i.e.: http://localhost:8080/3Ys
-config.webhost = 'https://shorturl-evilloria.c9users.io/';
+// for c9: https://shorturl-evilloria.c9users.io/
+config.webhost = 'https://shortyourl.herokuapp.com/';
 
 // your MongoDB host and database name
 config.db.host = 'localhost';
